025838cae412261897044b2c9ca567c3adf5dfa5
bsantanad <0211518@up.edu.mx>
2023-10-08 11:09:19 -0600

    home.php: landing page, modern style
    Signed-off-by: bsantanad <0211518@up.edu.mx>

--- templates/home.php
+++ templates/home.php
@@ -3,32 +3,40 @@
 <head>
     <meta charset="UTF-8">
     <title>русское слово</title>
-    <link rel="stylesheet" href="/static/css/style.css">
+    <link rel="stylesheet" href="/static/css/index.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
-    <header>
-      <h1>русское слово</h1>
-      <nav>
-        <ul>
-          <li><a href="/">дом</a></li>
-          <li><a href="/exercises">упражнений</a></li>
-        </ul>
-      </nav>
-    </header>
-    <main>
-        <article>
-            <h2><a href="/posts/prep-personal.html">
-                урок: prep. case for pronouns & что as a conjunction
-            </a></h2>
-            <small>октябрь 03, 2023</small>
-            <br>
-        </article>
-        <article>
-            <h2><a href="/posts/prepositions.html">урок: prepositions</a></h2>
-            <small>сентябрь 27, 2023</small>
-            <br>
-        </article>
-    </main>
+    <div class="container">
+        <div class="head">
+            <h1>слово</h1>
+        </div>
+        <div class="articles-container">
+            <div class="left-articles">
+            </div>
+            <div class="articles">
+                <ul>
+                    <li>
+                        <h3><a href='/' class='nav-title' id='active'>Уроки</a></h3>
+                        <h3>&nbsp ~ &nbsp</h3>
+                        <h3><a href="/exercises" class='nav-title'>Упражнений</a></h3>
+                    </li>
+                    <hr>
+                    <li>
+                        <a href="/posts/poss-acc.html">Accusative and Possessives</a>
+                        <a href="" class="right-note">Accusative</a>
+                    </li>
+                    <li>
+                        <a href="/posts/prep-personal.html">Что as a Conjunction</a>
+                        <a href="" class="right-note">Prepostions</a>
+                    </li>
+                    <li>
+                        <a href="/posts/prepositions.html">Basics on Prepostions</a>
+                        <a href="" class="right-note">Prepostions</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
